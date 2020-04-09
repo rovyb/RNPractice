54d9b144d7c9b5b50c7ffa105c80c0b7eb6791ea
Rory Balducci <rory@Rorys-MBP.fios-router.home>
2020-04-09 13:55:26 -0400

    2nd try

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={styles.text}>HomeScreen</Text>
+      <Button 
+        title="Go to Components Demo" 
+        onPress={() => navigation.navigate("ComponentsScreen")}
+        />
+      <Button 
+        title="Go to List Demo"
+        onPress={() => navigation.navigate("List")} />
+      
+    </View>
+  )
+};
+
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30
+  }
+});
+
+export default HomeScreen;
